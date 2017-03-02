35a5ff7a85c775e63ad9cfba47284ac74c305935
Evan Hickman <travisevanhickman@gmail.com>
2017-03-02 17:30:41 -0600

    style nav

---
+++ app/components/application.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div class="app">
+    <div class="section header">
+      <div class="container">
+        <nav class="top-nav">
+            <a href="#"><h1 class="top-nav__logo">Marvel</h1></a>
+        </nav>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
