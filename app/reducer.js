dd6d5607ad0e420b751e0c98e37f842c85657583
Evan Hickman <travisevanhickman@gmail.com>
2017-03-03 09:34:44 -0600

    more styling

---
+++ app/reducer.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux';
+import characterData from './reducer/character-data';
+import comicData from './reducer/comic-data';
+import modal from './reducer/modal';
+import seriesInfo from './reducer/series-info';
+
+export default combineReducers({
+  characterData,
+  comicData,
+  modal,
+  seriesInfo,
+});
