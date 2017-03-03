0a10e04a9ca50941ada9ac131b92c3e145897f1c
Evan Hickman <travisevanhickman@gmail.com>
2017-03-02 18:30:15 -0600

    install redux, redux thunk, and revue

--- app/components/application.vue
+++ app/components/application.vue
@@ -15,7 +15,7 @@
             <div><img src="http://placehold.it/200/2c3e50" alt="" class="series__details-image"></div>
             <h3 class="series__details-name">Apocalypse (2004 - 2009)</h3>
             <p class="series__details-years">(2004 - 2009)</p>
-            <h3 class="series__details-creators-heading">Creators</h3>
+            <h3 class="heading">Creators</h3>
             <p class="series__details-creator">Tom Brennan</p>
             <p class="series__details-creator">George Clooney</p>
             <p class="series__details-creator">Meryl Streep</p>
@@ -24,54 +24,54 @@
           <div class="grid__item series__content">
             <!-- stuff on right -->
             <div class="characters">
-              <h2 class="character__heading">Characters</h2>
-              <div class="grid characters__grid">
-              <div class="grid__item character">
-                <div><img src="http://placehold.it/100/2c3e50" alt="" class="character__img"></div>
-                <h4 class="character__name">Red Hulk</h4>
+              <h2 class="heading">Characters</h2>
+              <div class="grid">
+              <div class="grid__item">
+                <div><img src="http://placehold.it/100/2c3e50" alt="" class="thumbnail"></div>
+                <h4 class="thumbnail__caption">Red Hulk</h4>
               </div>
-              <div class="grid__item character">
-                <div><img src="http://placehold.it/100/2c3e50" alt="" class="character__img"></div>
-                <h4 class="character__name">Spider Girl</h4>
+              <div class="grid__item">
+                <div><img src="http://placehold.it/100/2c3e50" alt="" class="thumbnail"></div>
+                <h4 class="thumbnail__caption">Spider Girl</h4>
               </div>
-              <div class="grid__item character">
-                <div><img src="http://placehold.it/100/2c3e50" alt="" class="character__img"></div>
-                <h4 class="character__name">Spider Boy</h4>
+              <div class="grid__item">
+                <div><img src="http://placehold.it/100/2c3e50" alt="" class="thumbnail"></div>
+                <h4 class="thumbnail__caption">Spider Boy</h4>
               </div>
-              <div class="grid__item character">
-                <div><img src="http://placehold.it/100/2c3e50" alt="" class="character__img"></div>
-                <h4 class="character__name">Spider Man</h4>
+              <div class="grid__item">
+                <div><img src="http://placehold.it/100/2c3e50" alt="" class="thumbnail"></div>
+                <h4 class="thumbnail__caption">Spider Man</h4>
               </div>
             </div>
             <div class="issues">
-              <h2 class="issues__heading">Issues</h2>
+              <h2 class="heading">Issues</h2>
               <div class="grid issues__grid">
                 <div class="grid__item issue">
-                  <div><img src="http://placehold.it/100/2c3e50" alt="" class="issue_img"></div>
-                  <h4 class="issue__number">#1</h4>
-                  <p class="issue__details">Spider Girl</p>
-                  <p class="issue__details">(2004)#1</p>
+                  <div><img src="http://placehold.it/100/2c3e50" alt="" class="thumbnail"></div>
+                  <h4 class="thumbnail__caption">#1</h4>
+                  <p class="thumbnail__details">Spider Girl</p>
+                  <p class="thumbnail__details">(2004)#1</p>
                   <button v-on:click="">Read More</button>
                 </div>
                 <div class="grid__item issue">
-                  <div><img src="http://placehold.it/100/2c3e50" alt="" class="issue_img"></div>
-                  <h4 class="issue__number">#1</h4>
-                  <p class="issue__details">Spider Girl</p>
-                  <p class="issue__details">(2004)#1</p>
+                  <div><img src="http://placehold.it/100/2c3e50" alt="" class="thumbnail"></div>
+                  <h4 class="thumbnail__caption">#1</h4>
+                  <p class="thumbnail__details">Spider Girl</p>
+                  <p class="thumbnail__details">(2004)#1</p>
                   <button v-on:click="">Read More</button>
                 </div>
                 <div class="grid__item issue">
-                  <div><img src="http://placehold.it/100/2c3e50" alt="" class="issue_img"></div>
-                  <h4 class="issue__number">#1</h4>
-                  <p class="issue__details">Spider Girl</p>
-                  <p class="issue__details">(2004)#1</p>
+                  <div><img src="http://placehold.it/100/2c3e50" alt="" class="thumbnail"></div>
+                  <h4 class="thumbnail__caption">#1</h4>
+                  <p class="thumbnail__details">Spider Girl</p>
+                  <p class="thumbnail__details">(2004)#1</p>
                   <button v-on:click="">Read More</button>
                 </div>
                 <div class="grid__item issue">
-                  <div><img src="http://placehold.it/100/2c3e50" alt="" class="issue_img"></div>
-                  <h4 class="issue__number">#1</h4>
-                  <p class="issue__details">Spider Girl</p>
-                  <p class="issue__details">(2004)#1</p>
+                  <div><img src="http://placehold.it/100/2c3e50" alt="" class="thumbnail"></div>
+                  <h4 class="thumbnail__caption">#1</h4>
+                  <p class="thumbnail__details">Spider Girl</p>
+                  <p class="thumbnail__details">(2004)#1</p>
                   <button v-on:click="">Read More</button>
                 </div>
               </div>
