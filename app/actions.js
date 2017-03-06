237bfdb8b66bf443bdb447ba2e937d969f9ee480
Evan Hickman <travisevanhickman@gmail.com>
2017-03-06 15:34:46 -0600

    more work on actions.js

--- app/actions.js
+++ app/actions.js
@@ -11,3 +11,25 @@ export function charactersFindAllComplete(data) {
     data,
   };
 }
+
+export function seriesInfoSearch(name) {
+  return (dispatch) => {
+    fetch(`http://marvel-is-broke.herokuapp.com/series?limit=1&titleStartsWith=${name}`)
+    .then(res => res.json())
+    .then((data) => {
+      const characters = data.data.results;
+    });
+    dispatch(charactersFindForId(characters));
+  };
+}
+
+export function charactersFindForId(id) {
+  return (dispatch) => {
+    fetch(`http://marvel-is-broke.herokuapp.com/series?limit=1&titleStartsWith=${id}`)
+    .then(res => res.json())
+    .then((data) => {
+      const characters = data.data.results;
+    });
+    dispatch(charactersFindForId(characters));
+  };
+}
